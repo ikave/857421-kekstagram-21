95a303d009cd7849d6037d45f9328c5234a0ee46
ikave <shiftgear@mail.ru>
2020-10-21 14:48:27 +0300

    Вносит исправления при загрузке изображения

--- js/main.js
+++ js/main.js
@@ -183,8 +183,12 @@ const renderBigPicture = function (item) {
   renderSocialComments();
 };
 
-renderBigPicture(bigPicture);
+// module4-task1
 
+const IMAGE_SCALE_MIN = 25;
+const IMAGE_SCALE_MAX = 100;
+const IMAGE_SCALE_STEP = 25;
+const BASE_EFFECT_LEVEL = 100;
 
 const uploadFile = document.querySelector(`#upload-file`);
 const uploadOverlay = document.querySelector(`.img-upload__overlay`);
@@ -192,67 +196,124 @@ const uploadClose = uploadOverlay.querySelector(`#upload-cancel`);
 const body = document.querySelector(`body`);
 const effectLevelLine = uploadOverlay.querySelector(`.effect-level__line`);
 const effectLevelPin = uploadOverlay.querySelector(`.effect-level__pin`);
-const effectLevelValue = uploadOverlay.querySelector(`.effect-level__value`);
+const effectLevelInput = uploadOverlay.querySelector(`.effect-level__value`);
 const uploadForm = document.querySelector(`.img-upload__form`);
 const uploadScale = uploadForm.querySelector(`.img-upload__scale`);
 const uploadPreview = uploadForm.querySelector(`.img-upload__preview img`);
-const hashtags = uploadForm.querySelector(`.text__hashtags`);
-let uploadScaleValue = uploadForm.querySelector(`.scale__control--value`).value;
+const hashtagInput = uploadForm.querySelector(`.text__hashtags`);
+const uploadScaleInput = uploadForm.querySelector(`.scale__control--value`);
 
-uploadScale.addEventListener(`click`, function (evt) {
+let effectLevel = BASE_EFFECT_LEVEL;
+
+const getEffectLevel = function (value) {
+  let level = value;
+  return level;
+};
+
+const setEffectLevel = function () {
+  effectLevel = Math.round(effectLevelPin.offsetLeft / effectLevelLine.offsetWidth * 100);
+  effectLevelInput.value = effectLevel;
+};
+
+const filterEffects = {
+  none: `none`,
+  chrome: `grayscale(${getEffectLevel(effectLevel) / 100})`,
+  sepia: `sepia(${getEffectLevel(effectLevel) / 100})`,
+  marvin: `invert(${getEffectLevel(effectLevel)}%)`,
+  phobos: `blur(${getEffectLevel(effectLevel) * 3 / 100}px)`,
+  heat: `brightness(${getEffectLevel(effectLevel) * 3 / 100})`,
+};
+
+const openUploadPopup = function () {
+  uploadOverlay.classList.remove(`hidden`);
+  body.classList.add(`modal-open`);
+  hashtagInput.value = ``;
+  uploadScaleInput.value = `100%`;
+  uploadPreview.style.transform = `scale(1)`;
+};
+
+const closeUploadPopup = function () {
+  uploadOverlay.classList.add(`hidden`);
+  body.classList.remove(`modal-open`);
+  uploadFile.value = ``;
+};
+
+const onScaleClick = function (evt, input) {
+  let value = parseInt(input.value, 10);
   if (evt.target.classList.contains(`scale__control--smaller`)) {
-    uploadScaleValue = parseInt(uploadScaleValue, 10) - 25;
-    if (uploadScaleValue <= 25) {
-      uploadScaleValue = 25;
+    value -= IMAGE_SCALE_STEP;
+    input.value = `${value}%`;
+    if (value <= IMAGE_SCALE_MIN) {
+      value = IMAGE_SCALE_MIN;
+      input.value = `${value}%`;
     }
-    uploadPreview.style.transform = `scale(${uploadScaleValue / 100})`;
+    uploadPreview.style.transform = `scale(${value / 100})`;
   }
 
   if (evt.target.classList.contains(`scale__control--bigger`)) {
-    uploadScaleValue = parseInt(uploadScaleValue, 10) + 25;
-    if (uploadScaleValue > 100) {
-      uploadScaleValue = 100;
+    value += IMAGE_SCALE_STEP;
+    input.value = `${value}%`;
+    if (value > IMAGE_SCALE_MAX) {
+      value = IMAGE_SCALE_MAX;
+      input.value = `${value}%`;
     }
-    uploadPreview.style.transform = `scale(${uploadScaleValue / 100})`;
+    uploadPreview.style.transform = `scale(${value / 100})`;
   }
-});
+};
+
+const pressEscKey = function (evt) {
+  if (evt.key === `Escape`) {
+    closeUploadPopup();
+  }
+};
 
 uploadFile.addEventListener(`change`, function () {
-  uploadOverlay.classList.remove(`hidden`);
-  body.classList.add(`modal-open`);
+  openUploadPopup();
+
+  uploadScale.addEventListener(`click`, function (evt) {
+    onScaleClick(evt, uploadScaleInput);
+  });
+
+  effectLevelPin.addEventListener(`mouseup`, function () {
+    setEffectLevel();
+  });
+
+  uploadForm.addEventListener(`change`, function (evt) {
+    if (evt.target.type === `radio`) {
+      effectLevel = BASE_EFFECT_LEVEL;
+      effectLevelInput.value = effectLevel;
+      for (let effect in filterEffects) {
+        if (evt.target.value === effect) {
+          uploadPreview.style.filter = filterEffects[effect];
+        }
+      }
+    }
+  });
+
+  hashtagInput.addEventListener(`focus`, function () {
+    window.removeEventListener(`keydown`, pressEscKey);
+  });
+
+  hashtagInput.addEventListener(`blur`, function () {
+    window.addEventListener(`keydown`, pressEscKey);
+  });
+
+  hashtagInput.addEventListener(`input`, function () {
+    let hashtags = hashtagInput.value.split(` `);
+    const reg = /^#[\w]*$/;
+    for (let hashtag of hashtags) {
+      if (!reg.test(hashtag)) {
+        hashtagInput.setCustomValidity(`Неверный хештег`);
+      } else {
+        hashtagInput.setCustomValidity(``);
+      }
+    }
+  });
 
   uploadClose.addEventListener(`click`, function () {
-    uploadOverlay.classList.add(`hidden`);
-    body.classList.remove(`modal-open`);
-    uploadFile.value = ``;
+    closeUploadPopup();
   });
 
-  window.addEventListener(`keydown`, function (evt) {
-    if (evt.key === `Escape`) {
-      uploadOverlay.classList.add(`hidden`);
-      body.classList.remove(`modal-open`);
-      uploadFile.value = ``;
-    }
-  });
-});
+  window.addEventListener(`keydown`, pressEscKey);
 
-effectLevelPin.addEventListener(`mouseup`, function () {
-  effectLevelValue.value = Math.round(effectLevelPin.offsetLeft / effectLevelLine.offsetWidth * 100);
-
-});
-
-uploadForm.addEventListener(`change`, function (evt) {
-  if (evt.target.type === `radio`) {
-    effectLevelValue.value = 100;
-  }
-});
-
-hashtags.addEventListener(`input`, function () {
-  let arr = hashtags.value.split(` `);
-  const reg = /^#[\w]*$/;
-  for (let hashtag of arr) {
-    if (!reg.test(hashtag)) {
-      hashtags.setCustomValidity(`Неверный хештег`);
-    }
-  }
 });
